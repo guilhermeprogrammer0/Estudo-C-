e7aa0a86a5a1d2186977ba67ddd8c0a1e87be4e3
GUILHERME  DA SILVA SOUZA <0220482213027@fatectq.local>
2022-08-11 20:23:37 -0300

    Adicionado ao github

---
+++ programateste.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int matriz[2][2];
+    int i,j;
+
+    for(i=0;i<2;i++)
+    {
+
+        for(j=0;j<2;j++)
+        {
+
+            printf("\n Insira valores para %d e %d ",i, j);
+            scanf("%d%*c",&matriz[i][j]);
+
+
+        }
+
+    }
+    for(i=0;i<2;i++)
+    {
+
+        for(j=0;j<2;j++)
+        {
+
+            printf("\n %d ",matriz[i][j]);
+
+
+
+        }
+
+    }
+
+
+
+
+
+    return (0);
+}
